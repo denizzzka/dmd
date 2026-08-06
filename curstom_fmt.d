3339473aebf163a6bd0fb9c8a4dd7ca28f57d9f9
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-06 16:35:53 +0300

    broken test message improved

--- curstom_fmt.d
+++ curstom_fmt.d
@@ -19,7 +19,7 @@ void main()
         static void testIt(T)(T val, const bool expForm, const char[] expected, size_t line = __LINE__)
         {
             auto res = num2ascii(val, expForm);
-            assert(res == expected, "Test at line "~line.num2ascii~": "~res);
+            assert(res == expected, "Test at line "~line.num2ascii~`: "`~res~`", but expected: "`~expected~`"`);
         }
 
         //TODO: add test with leading zero
