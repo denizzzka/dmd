f6e98e29b497f9c175ccd0af098dba605ccc0a4f
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-13 14:50:42 +0300

    sink() supports strings and IES arg

--- druntime/src/core/internal/sink.d
+++ druntime/src/core/internal/sink.d
@@ -2,19 +2,36 @@ module core.internal.sink;
 
 import core.interpolation;
 
-void sink(IES...)(in IES ies) //nothrow @nogc @safe
+void sink(IES...)(IES ies) //nothrow @nogc @safe
+if(is(IES[0] == InterpolationHeader) || is(IES[0] == string) && IES.length == 1)
 {
+    import core.stdc.stdio: fflush, fprintf, fwrite, stderr, stdout;
+    import core.stdc.stdlib: abort;
+
     static foreach(e; ies)
     {
-        import std.stdio;
-        e.write;
+        static if(is(typeof(e) == struct))
+        {
+            pragma(msg, "Unsupported expression : " ~ typeof(e).stringof);
+        }
+        else static if(is(typeof(e) == string) || __traits(isSame, typeof(e), InterpolatedLiteral))
+        {
+            auto r = fwrite(e.ptr, char.sizeof, e.length, /*cast()*/ stdout);
+            if(r < e.length)
+                abort();
+        }
+        else
+            static assert(false, "Unsupported IES expression type: " ~ typeof(e).stringof);
     }
+
+    fflush(stdout);
 }
 
 unittest
 {
     enum hello = "Hello, world!";
 
+    sink(`atr`);
     sink(i`sink() says: "$(hello)"`);
 
     /*
