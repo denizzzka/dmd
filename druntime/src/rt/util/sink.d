f3a00f72a52e663a33f317251f1ff214f5d1da42
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-13 13:07:14 +0300

    rt.sink module added

---
+++ druntime/src/rt/util/sink.d
@@ -0,0 +1,28 @@
+module rt.sink;
+
+import core.interpolation;
+
+enum name = "John Doe";
+auto items = i"Hello, $(name), how are you?";
+
+unittest
+{
+    /+
+    assert(
+        items == tuple(
+            InterpolationHeader(),                       // denotes the start of an IES
+            InterpolatedLiteral!("Hello, ")(),           // literal string data
+            InterpolatedExpression!("name")(),           // expression literal data
+            name,                                        // expression passed directly
+            InterpolatedLiteral!(", how are you?")(),    // literal string data
+            InterpolationFooter()
+        )
+    );
+    +/
+
+    import std.stdio;
+    foreach(e; items[1 .. $-1])
+        e.write;
+
+    writeln;
+}
