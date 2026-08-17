48afbd0473e3164889c35d8612e752e49b092575
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-17 11:21:05 +0300

    latest test enabled

--- druntime/src/core/internal/ftot.d
+++ druntime/src/core/internal/ftot.d
@@ -218,7 +218,7 @@ unittest
     onAll(1000.0f, "1000", "1.0e+03" /* FIXME: should be 1e+03 */);
     onAll(0.001f, "0.001", "1.0e-03");
     onAll(0.0001f, "0.0001", "1.0e-04");
-    //~ onAll(double(-1.0e-8), "errrr 111", "errrr xxxxx");
+    onAll(double(-1.0e-8), "FIXME: not implemented!", "-1.0e-08");
 }
 
 /**
