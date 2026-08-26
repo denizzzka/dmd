b16bf8d44342f0cb8f175508e63397d727160507
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-26 21:04:45 +0300

    bigitsArrLength: .mant_dig -> .dig, swap left and right

--- druntime/src/core/internal/ftot/decimal.d
+++ druntime/src/core/internal/ftot/decimal.d
@@ -58,9 +58,9 @@ if(__traits(isFloating, F))
 
     private
     {
-        enum totalDigits = 1 /* digit before dot */ + F.max_10_exp + (-F.min_10_exp) + F.mant_dig;
-        enum maxLeftShifts = (-F.min_10_exp + decimalExp-1) / decimalExp;
-        enum maxRightShifts = (F.max_10_exp + decimalExp-1) / decimalExp;
+        enum totalDigits = 1 /* digit before dot */ + F.max_10_exp + (-F.min_10_exp) + F.dig;
+        enum maxLeftShifts = (F.max_10_exp + decimalExp-1) / decimalExp;
+        enum maxRightShifts = (-F.min_10_exp + decimalExp-1) / decimalExp;
         enum maxShifts = maxLeftShifts < maxRightShifts ? maxRightShifts : maxLeftShifts;
     }
 
